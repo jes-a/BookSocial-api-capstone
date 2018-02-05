0a0eed3557423e827c1e6c429be79fbebc15b413
Jes Albro <jesalbro@gmail.com>
2018-02-05 14:40:24 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This app allows users to search for literary events in their area using various event APIs
---
+++ main.js
@@ -0,0 +1,6 @@
+const GOOGLE_MAPS_SEARCH_URL = ""
+	  MEETUP_SEARCH_URL = "https://api.meetup.com/find/upcoming_events",
+	  GOODREADS_SEARCH_URL = "https://www.goodreads.com/event/index.xml",
+	  EVENTBRITE_SEARCH_URL = ""
+
+	  
